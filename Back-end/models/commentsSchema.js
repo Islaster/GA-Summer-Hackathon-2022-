366c1b85f80c647f714b454a8a32281f5653d585
kbrpronet <kaibrown2889@gmail.com>
2022-08-09 20:20:42 -0400

    made small changes to schema, removed duplicate code

--- Back-end/models/commentsSchema.js
+++ Back-end/models/commentsSchema.js
@@ -3,8 +3,7 @@ const mongoose = require('mongoose');
 const commentSchema = new mongoose.Schema ({
     name: String, 
     comment: String,
-    created_at: {type: Date, default: Date.now},
-    updated_at: {type: Date, default: Date.now}
+    timestamps: true
 })
 
 const comments = mongoose.model('comment', commentsSchema);
--- Back-end/models/postsSchema.js
+++ Back-end/models/postsSchema.js
@@ -1,11 +1,10 @@
-<<<<<<< HEAD
 const mongoose = require('mongoose');
 
 const postSchema = new mongoose.Schema ({
-    created_at: {type: Date, default: Date.now},
+    timestamps: true,
     name: {type: String, required: true},
-    location: {type: String, required: true},
-    dateAvailable: {type: Date, required: true} 
+    zipCode: {type: String, required: true},
+    dateAvailable: {type: Date, required: true}, 
     food: {type: String, required: true},
     description: {type: String, required: true},
     allergens: {type: Boolean, required: True}
@@ -13,19 +12,3 @@ const postSchema = new mongoose.Schema ({
 
 const posts = mongoose.model('post', postSchema);
 module.exports = Posts;
-=======
-const mongoose = require("mongoose");
-
-const postSchema = new mongoose.Schema({
-  created_at: { type: Date, default: Date.now },
-  name: { type: String, required: true },
-  location: { type: String, required: true },
-  dateAvailable: { type: Date, required: true },
-  food: { type: String, required: true },
-  description: { type: String, required: true },
-  allergens: { type: Boolean, required: True },
-});
-
-const Posts = mongoose.model("post", postSchema);
-module.exports = Posts;
->>>>>>> 62f99f3c5ba2ae5bd671d757aaeb99d4429f9681
