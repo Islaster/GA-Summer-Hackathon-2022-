080f464ec87c861146b35383d79f4c1a8682c1f8
Navid Yazdani <mohammadYazdani7@gmail.com>
2022-08-09 21:53:35 -0700

    login page

--- Front-End/codequackers/src/pages/profile/Profile.js
+++ Front-End/codequackers/src/pages/profile/Profile.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from 'react';
+import { useAuth0 } from '@auth0/auth0-react';
+import JSONPretty from 'react-json-pretty';
 
-function Profile() {
-  return (
-    <div>Profile</div>
-  )
-}
+function Profile () {
+	const { user, isAuthenticated } = useAuth0();
 
-export default Profile
+	return (
+		isAuthenticated &&
+		<div>
+			<img src={user.picture} alt={user.name} />
+			<h2>
+				{user.name}
+			</h2>
+			<p>
+				{user.email}
+			</p>
+			<JSONPretty data={user} />
+			{/* {JSON.stringify(user, null, 2)} */}
+		</div>
+	);
+};
+
+export default Profile;
